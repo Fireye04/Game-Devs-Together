44e0e8b12060e44a4dc24125cb892dd2a44f5bbc
Fireye <codekai16@gmail.com>
2025-12-30 15:49:51 -0700

    Polish widget script updates

--- assets/scripts/widget.js
+++ assets/scripts/widget.js
@@ -1,17 +1,46 @@
+//Code originally adapted from https://transring.neocities.org/ring.js
 import * as data from "members.json";
-import rainbowDarkWidget from "/Assets/widget-rainbow-dark.html?raw";
+import rainbowDarkWidget from "/Assets/Widgets/rainbow-dark.svg";
+import originalWidget from "/Assets/Widgets/original.svg";
 
-//Code adapted from https://transring.neocities.org/
+/*
+ * Script Inputs
+ * =============
+ * "widget-style" - User widget style of choice. Either "rainbow-dark" or "original".
+ *
+ *
+ * Widget Consts
+ * -------------
+ * {webring-link} - Link referring back to the GDT website. Currently https://gdt.fireye.coffee.
+ * {alt-text} - Alt text descriptor for the widget.
+ * {prev-link} - Link to previous GDT member.
+ * {next-link} - link to next GDT member.
+ */
+
+/************** WIDGET INPUT **************/
+
+// Apply identical wrappers to all widgets
+function getWrapperHTML(rawWidget){
+    return `<div id='GDT-Widget'>${rawWidget}</div>`;
+}
+
+function getErrorWrapperHTML(rawWidget){
+    return `<div id="GDT-Widget-Error">
+        ${rawWidget}
+        <span id="GDT-Widget-Error-Message">
+			This site is not yet a GDT member :)
+		</span></div>`;
+}
+
+
 widgets = {
 	"rainbow-dark": rainbowDarkWidget,
-	"original": `
-		<div id="GDT-Widget">
-			<a href="{webring-link}" target="_blank"><img src="https://gdt.fireye.coffee/Assets/GDT.svg" alt="{alt-text}" width=200></a>
-		</div>
-	`,
+	"original": originalWidget,
 };
 
 style = document.currentScript.getAttribute("widget-style");
+
+// User input handling
 if (style === null) {
 	style = "rainbow-dark";
 }
@@ -22,7 +51,10 @@ if (!(style in widgets)) {
 
 html = widgets[style];
 
-//Include this site by default
+
+/************** MEMBER LIST RESOLUTION **************/
+
+//Include GDT website by default
 siteurls = ["gdt.fireye.coffee"];
 
 for (const site of data.sites) {
@@ -31,45 +63,42 @@ for (const site of data.sites) {
 
 window.data = siteurls;
 
-console.log(window.data);
-
 idx = window.data.findIndex((site) =>
 	location.href
 		.replace(/https:\/\/|http:\/\/|\/.*/gm, "")
 		.startsWith(site, 0),
 );
 
+// Remove GDT website from member URLs
+window.data.splice(0,1);
+
 webringLink = "https://gdt.fireye.coffee";
 
+// Site not found in member list 
 if (idx === -1) {
-	html = html
+    // Set error wrapper
+	html = getErrorWrapperHTML(html
 		.replace("{webring-link}", webringLink)
 		.replace("{prev-link}", webringLink)
 		.replace("{next-link}", webringLink)
-		.replace("{alt-text}", "");
-	html = `
-	<div id="gdt-widget-error">
-		${html} 
-		<span id="gdt-widget-error-message">
-			This site is not yet a GDT member :)
-		</span>
-	</div>
-	`;
-	document.currentScript.outerHTML = html;
-	throw new Error("This site is not a GDT member");	
-}
+		.replace("{alt-text}", "Game Devs Together webring widget. This site is not a member."));
+    document.currentScript.outerHTML = html;
 
+    // return early
+	throw new Error("This site is not a GDT member");	
+} 
+
+// Assemble next and previous member links
 prevIdx = (idx - 1) % window.data.length;
 nextIdx = (idx + 1) % window.data.length;
-
 
 prevLink = "https://" + window.data[prevIdx];
 nextLink = "https://" + window.data[nextIdx];
 
-html = html
-	.replace("{webring-link}", webringLink)
-	.replace("{prev-link}", prevLink)
-	.replace("{next-link}", nextLink)
-	.replace("{alt-text}", "Game Devs Together Member!");
+html = getWrapperHTML(html
+    .replace("{webring-link}", webringLink)
+    .replace("{prev-link}", prevLink)
+    .replace("{next-link}", nextLink)
+    .replace("{alt-text}", "Game Devs Together webring widget. This site is a member!"));
 
 document.currentScript.outerHTML = html;
